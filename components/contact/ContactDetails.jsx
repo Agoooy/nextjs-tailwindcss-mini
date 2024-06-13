65b5a5fb2f354f62b40f43d6e178666547c89223
Yogaa12 <you@example.com>
2024-06-13 11:17:10 +0700

    modified:  components/contact/ContactDetails.jsx modified: components/shared/AppBanner.jsx modified: public/files/Stoman-Resume.pdf modified: public/files/Yoga-Resume.pdf

--- components/contact/ContactDetails.jsx
+++ components/contact/ContactDetails.jsx
@@ -3,17 +3,17 @@ import { FiPhone, FiMapPin, FiMail } from 'react-icons/fi';
 const contacts = [
 	{
 		id: 1,
-		name: 'Your Address, Your City, Your Country',
+		name: 'Surabaya, Jawa Timur, Indonesia',
 		icon: <FiMapPin />,
 	},
 	{
 		id: 2,
-		name: 'email@domain.com',
+		name: 'yogaardikaaa123@gmail.com',
 		icon: <FiMail />,
 	},
 	{
 		id: 3,
-		name: '555 8888 888',
+		name: '+62-896-8102-4466',
 		icon: <FiPhone />,
 	},
 ];
