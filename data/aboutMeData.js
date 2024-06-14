759583518f1d07fd39b08b334f00ef09fc233133
Yogaa12 <you@example.com>
2024-06-14 19:03:12 +0700

    data/aboutMeData.js

--- data/aboutMeData.js
+++ data/aboutMeData.js
@@ -3,10 +3,10 @@ import { v4 as uuidv4 } from 'uuid';
 export const aboutMeData = [
 	{
 		id: uuidv4(),
-		bio: 'Hi, I&apos;m Yoga, a passionate full-stack developer and UI/UX designer currently pursuing my sstudies as a university student. With a strong foundation in both front-end and back-end development, I strive to create seamless, user-friendly digital experiences. My expertise in UI/UX design allows me to bridge the gap between aesthetics and functionality, ensuring that every project I work on is both visually appealing and highlu functional.',
+		bio: 'Hi, I am Yoga, a passionate full-stack developer and UI/UX designer currently pursuing my sstudies as a university student. With a strong foundation in both front-end and back-end development, I strive to create seamless, user-friendly digital experiences. My expertise in UI/UX design allows me to bridge the gap between aesthetics and functionality, ensuring that every project I work on is both visually appealing and highlu functional.',
 	},
 	{
 		id: uuidv4(),
-		bio: 'I&apos;m alwayas eager to learn and grow, constantly seeking new challenges and opportunities to expand my skill set.',
+		bio: 'I am alwayas eager to learn and grow, constantly seeking new challenges and opportunities to expand my skill set.',
 	},
 ];
